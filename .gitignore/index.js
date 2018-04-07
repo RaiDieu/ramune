a14692ef340086d54fe9b983e67933b95d2e6747
RaiDieu <38014167+RaiDieu@users.noreply.github.com>
2018-04-07 20:23:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -120,5 +120,19 @@ bot.on('message', message => {
     }
 
 })
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        var embed = new Discord.RichEmbed()
+            .setTitle("Commande disponible :")
+            .setDescription(".help")
+            .setDescription("comming soon !")
+            .setThumbnail("https://cdn.discordapp.com/attachments/431694340830789645/431977745145266176/tumblr_o4ug71pfSH1tydz8to1_540.gif")
+            .setColor("0x2A933C")
+        message.channel.sendEmbed(embed);
+    }
+
+})
+
+
  
 
